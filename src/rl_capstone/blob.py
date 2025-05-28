82ffdda84cb5577d537a1d70697abc871470e263
Andrew Falcone <falcone.andrew@gmail.com>
2025-05-27 22:41:11 -0600

    Update README and BlobEnv demo

--- src/rl_capstone/blob.py
+++ src/rl_capstone/blob.py
@@ -44,15 +44,22 @@ class Blob:
         elif choice == 8:
             self.move(x=0, y=0)
 
-    def move(self, x=False, y=False):
+    def move(self, x=None, y=None):
+        """Move the blob.
+
+        If ``x`` or ``y`` is ``None``, the respective coordinate will move in a
+        random direction ``[-1, 0, 1]``. A value of ``0`` means no movement in
+        that axis.
+        """
+
         # If no value for x, move randomly
-        if not x:
+        if x is None:
             self.x += np.random.randint(-1, 2)
         else:
             self.x += x
 
         # If no value for y, move randomly
-        if not y:
+        if y is None:
             self.y += np.random.randint(-1, 2)
         else:
             self.y += y
@@ -203,3 +210,18 @@ class BlobEnv:
             cv2.waitKey(1)  # needed to actually close image, could just leave image up too?
         else:
             cv2.waitKey(pause)
+
+def main():
+    """Run a simple random policy in the Blob environment."""
+    env = BlobEnv()
+    env.reset()
+    env.render()
+    done = False
+    while not done:
+        action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+        _, _, done = env.step(action)
+        env.render()
+
+
+if __name__ == "__main__":
+    main()
